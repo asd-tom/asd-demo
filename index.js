0f8c650f0c8618b266105c406a2bbdb3b7e6845b
Tom Kr. Bardal <tom@tta-sound.com>
2019-08-15 18:08:29 +0200

    Manages to read samples from a time interval

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var headers = {
 $.ajax({
   url: 'https://api.nrfcloud.com/v1/messages',
   method: 'get',
-  data: '?inclusiveStart=2019-08-13T00%3A00%3A00.000Z&exclusiveEnd=2019-08-16T23%3A59%3A59.999Z',
+  data: 'inclusiveStart=2019-08-13T00:00:00.000Z&exclusiveEnd=2019-08-15T13:59:59.999Z',
   headers: headers,
   success: function(data) {
     console.log(JSON.stringify(data));
